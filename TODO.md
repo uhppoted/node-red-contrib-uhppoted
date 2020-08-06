229136c9a1300b54bfdc46aec24b5aad56c6b994
Tony Seebregts <tony.seebregts@gmail.com>
2020-08-05 21:50:29 -0700

    Promisified get-devices

--- TODO.md
+++ TODO.md
@@ -6,13 +6,14 @@
       - https://stackoverflow.com/questions/59762330/nodejs-brooadcast-udp
       - https://stackoverflow.com/questions/3653065/get-local-ip-address-in-node-js
 - [ ] Decode get-devices reply
-- [ ] Terminate early on send error
 - [ ] Add bind address to node properties
 - [ ] Add broadcast address to node properties
 - [ ] Add timeout to node properties
-- [ ] Figure out how to use Promises rather than callbacks for UDP
-- [ ] 'catch' promise errors
 - [ ] ESLint
+
+- [x] Terminate early on send error
+- [x] Figure out how to use Promises rather than callbacks for UDP
+- [x] 'catch' promise errors
 
 ## TODO
 
--- nodes/get-devices.js
+++ nodes/get-devices.js
@@ -1,20 +1,20 @@
-
 module.exports = function(RED) {    
     function GetDevicesNode(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
+        RED.nodes.createNode(this, config);
+        
+        const node = this;
+        
         node.on('input', function(msg) {
-            let request  = [ 0x17, 0x94 ];
-            
-            broadcast(this, request).then(reply => {
-                let devices = [];
+            const request = [ 0x17, 0x94 ];
 
+            let decode = function(reply) {
+                const devices = [];
                 reply.forEach(element => {
                     if ((element.length == 64) && (element[0] == 0x17) && (element[1] == 0x94)) {
-                        let v = new DataView(element.buffer);
+                        let bytes  = new DataView(element.buffer);
                         let device = { 
                             device: {
-                                id: v.getUint32(4,true)
+                                id: bytes.getUint32(4, true)
                                 // IpAddress    net.IP             `uhppote:"offset:8"`
                                 // SubnetMask   net.IP             `uhppote:"offset:12"`
                                 // Gateway      net.IP             `uhppote:"offset:16"`
@@ -26,60 +26,70 @@ module.exports = function(RED) {
 
                         devices.push(device);
                     }
-                })
+                });
+                
+                return devices
+            }
 
+            let emit = function(devices) {
                 msg.payload = devices;
                 node.send(msg);
-            });
+            }
+
+            broadcast(this, request, 5000)
+                .then(reply   => { return decode(reply) })
+                .then(devices => { return emit(devices) })
+                .catch(err    => { node.error('uhppoted::broadcast  ' + err) });
         });
     }
 
     RED.nodes.registerType('get-devices', GetDevicesNode);
 }
 
-async function broadcast(node, request) {
+function broadcast(node, request, timeout) {
     const dgram = require('dgram');
     const opts  = { type: 'udp4', reuseAddr:true };
     const sock  = dgram.createSocket(opts);
-    const rq = new Uint8Array(64);
+    const reply = [];
+    const rq    = new Uint8Array(64);
 
     rq.set(request);
 
-    let reply = [];
+    return new Promise(async (resolve, reject) => {        
+        const send = function() {
+            sock.send(rq, 0, rq.length, 60000, '255.255.255.255', (err, bytes) => {
+                if (err) {
+                    reject(err);                    
+                }
+            });
+        }
 
-    sock.on('error', function(err) {
-        node.error('uhppoted::broadcast  ' + err);
-    });
+        const received = function(message) {
+            reply.push(new Uint8Array(message));
+        }
 
-    sock.on('message', function (message, remote) {
-        reply.push(new Uint8Array(message));
-    });
+        const bound = function() {
+            sock.setBroadcast(true);
+            send();
+        }
 
-    sock.on('listening', () => {
-        node.log('uhppoted::broadcast.listening  ' + sock.address().address + ':' + sock.address().port);
-        sock.setBroadcast(true);
+        const error = function(err) {
+            reject(err);
+        }
 
-        sock.send(rq, 0, rq.length, 60000, '255.255.255.255', function(err, bytes) {
-            if (err) {
-                node.error('uhppoted::broadcast  ' + err);
-            }
-        });
-    });    
-    
-    sock.bind();
+        sock.on('listening', () => { bound(); });            
+        sock.on('message',   (message, remote) => { received(message) });
+        sock.on('error',     (err)  => { error(err) });
+        sock.bind();
 
-    await new Promise(resolve => setTimeout(resolve, 2500));
-
-    close(sock);
-    
-    return reply
+        await wait(timeout);
+        
+        resolve(reply);
+        sock.close();
+    })
 }
 
-function close(sock) {
-    try {
-        sock.close();
-    } catch (err) {
-        node.error('uhppoted::close  ' + err);
-    }
+function wait(timeout) {
+    return new Promise(resolve => setTimeout(resolve, timeout));
 }
 
