787835bf0191661feb3783c4ce40799781a20ebc
Tony Seebregts <tony.seebregts@gmail.com>
2020-09-03 10:31:23 -0700

    Updated get-device to use uhppoted.get

--- nodes/get-device.js
+++ nodes/get-device.js
@@ -1,6 +1,5 @@
 module.exports = function (RED) {
   const uhppoted = require('./uhppoted.js')
-  const codec = require('./codec.js')
 
   function GetDeviceNode (config) {
     RED.nodes.createNode(this, config)
@@ -8,58 +7,27 @@ module.exports = function (RED) {
     const node = this
     const uhppote = RED.nodes.getNode(config.uhppote)
 
-    let timeout = 5000
-    let bind = '0.0.0.0'
-    let dest = '255.255.255.255:60000'
-    let debug = false
-
-    if (uhppote) {
-      timeout = uhppote.timeout
-      bind = uhppote.bind
-      dest = uhppote.broadcast
-      debug = uhppote.debug ? function (l, m) { node.log(l + '\n' + m) } : null
-    }
-
-    this.status({})
+    node.status({})
 
     this.on('input', function (msg, send, done) {
       const deviceId = msg.payload.deviceId
 
-      const decode = function (deviceid, reply) {
-        if (reply) {
-          const response = codec.decode(reply)
-
-          if (response && (response.deviceId === deviceId) && response.device) {
-            return response
-          }
-        }
-
-        throw new Error(`no reply to get-device request for device ID ${deviceId}`)
-      }
-
-      const emit = function (device) {
-        msg.payload = device
+      const emit = function (object) {
+        msg.payload = object
         send(msg)
       }
 
-      try {
-        const request = codec.encode(0x94, deviceId)
-
-        uhppoted.execute(bind, dest, request, timeout, debug)
-          .then(reply => { return decode(this.deviceid, reply) })
-          .then(object => { return emit(object) })
-          .then(done())
-          .catch(err => {
-            node.status({ fill: 'red', shape: 'dot', text: 'error' })
-            node.warn('uhppoted::execute  ' + err)
-          })
-      } catch (err) {
+      const error = function (err) {
         node.status({ fill: 'red', shape: 'dot', text: 'error' })
         node.warn('uhppoted::execute  ' + err)
       }
-    })
 
-    this.on('close', function () {
+      try {
+        uhppoted.get(deviceId, 0x94, {}, uhppote, (m) => { node.log(m) })
+          .then(object => { emit(object) })
+          .then(done())
+          .catch(err => { error(err) })
+      } catch (err) { error(err) }
     })
   }
 
