9c20f2a0518da2038391831e8545ba271b9e18f6
twystd <development@twyst.co.za>
2022-08-25 14:22:12 -0700

    Created locale entries for all uhppoted- nodes

---
+++ nodes/uhppoted-set-ip.js
@@ -0,0 +1,46 @@
+module.exports = function (RED) {
+  const common = require('./common.js')
+  const uhppoted = require('./uhppoted.js')
+  const opcodes = require('../nodes/opcodes.js')
+
+  function SetIPNode (config) {
+    RED.nodes.createNode(this, config)
+
+    const node = this
+    const topic = config.topic
+    const uhppote = RED.nodes.getNode(config.config)
+
+    this.status({})
+
+    this.on('input', function (msg, send, done) {
+      const t = (topic && topic !== '') ? topic : msg.topic
+      const deviceId = msg.payload.deviceId
+      const address = msg.payload.address
+      const netmask = msg.payload.netmask
+      const gateway = msg.payload.gateway
+
+      const emit = function (object) {
+        common.emit(node, t, object)
+      }
+
+      const error = function (err) {
+        common.error(node, err)
+      }
+
+      try {
+        const context = {
+          config: uhppote,
+          translator: (k) => { return RED._('set-ip.' + k) },
+          logger: (m) => { node.log(m) }
+        }
+
+        uhppoted.send(context, deviceId, opcodes.SetIP, { address: address, netmask: netmask, gateway: gateway })
+          .then(object => { emit(object) })
+          .then(done())
+          .catch(err => { error(err) })
+      } catch (err) { error(err) }
+    })
+  }
+
+  RED.nodes.registerType('uhppoted-set-ip', SetIPNode)
+}
